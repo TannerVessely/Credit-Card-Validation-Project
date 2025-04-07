d08e2689f56f0d952db01f38ccc54954709998f0
tanner.vessely <tanner.vessely@MU-C1-10.mullen.pvt.k12.co.us>
2025-04-07 10:33:08 -0600

    innitail Commit

---
+++ src/ValidateCard.java
@@ -0,0 +1,13 @@
+
+public class ValidateCard
+	{
+
+		public static void main(String[] args)
+			{
+			
+				for(int i; CreditCardNums.lengeth; i++) 
+	{
+	//gith
+}
+			}
+	}
---
+++ src/filereader.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class filereader
+	{
+
+		private static ArrayList<ValidateCard> CreditCardNums = new ArrayList<ValidateCard>();
+		private static String fileName = "Cardnums.txt";
+		
+		public static void runFileReader()
+		{
+			
+	
+				
+			}
+		}
+	
